22c389e7f640a147db1cd9a3b3974f9df1356999
Nuwan Dias <nuwand@wso2.com>
2018-06-27 15:48:37 +0530

    Changing version to 1.0 and separating Spring Application from RestController

--- helloworld/src/main/java/com/manning/mssecurity/helloworld/HelloService.java
+++ helloworld/src/main/java/com/manning/mssecurity/helloworld/HelloService.java
@@ -1,22 +1,16 @@
 package com.manning.mssecurity.helloworld;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@SpringBootApplication
+@RequestMapping("/hello")
 public class HelloService {
 
-    public static void main(String args[]) {
-        
-        SpringApplication.run(HelloService.class, args);
-    }
-
-    @RequestMapping("/hello")
+    @GetMapping
     public String sayHello(@RequestParam("name") String name) {
 
         return "Hello " + (StringUtils.isEmpty(name) ? "Spring Boot!!" : name + "!!");
