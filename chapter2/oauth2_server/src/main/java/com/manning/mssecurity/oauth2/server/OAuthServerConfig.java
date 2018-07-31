e7b7dfa46a84b2c1cf47b947b9a02d8e1a915d09
Nuwan Dias <nuwand@wso2.com>
2018-08-01 00:48:03 +0530

    Organizing samples under chapters

---
+++ chapter2/oauth2_server/src/main/java/com/manning/mssecurity/oauth2/server/OAuthServerConfig.java
@@ -0,0 +1,49 @@
+package com.manning.mssecurity.oauth2.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private TokenStore tokenStore;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .authenticationManager(authenticationManager)
+                .approvalStoreDisabled()
+                .tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient("applicationid")
+                .secret("applicationsecret")
+                .authorizedGrantTypes("client_credentials", "password")
+                .scopes("read", "write")
+                .accessTokenValiditySeconds(300)
+                .resourceIds("sample-oauth");
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .checkTokenAccess("isAuthenticated()")
+                .allowFormAuthenticationForClients();
+    }
+
+}
