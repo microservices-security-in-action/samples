06fe05201769acf05712ec09eca047c4e3146d77
Prabath Siriwardana <prabath@wso2.com>
2018-12-12 06:17:06 -0500

    updates

---
+++ chapter06/sample01/src/main/java/com/manning/mss/ch06/sample01/service/OrderProcessingService.java
@@ -0,0 +1,56 @@
+package com.manning.mss.ch06.sample01.service;
+
+import java.net.URI;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.manning.mss.ch06.sample01.model.Item;
+import com.manning.mss.ch06.sample01.model.Order;
+import com.manning.mss.ch06.sample01.model.PaymentMethod;
+
+@RestController
+@RequestMapping(value = "/orders")
+public class OrderProcessingService {
+
+	@RequestMapping(value = "/{id}/status", method = RequestMethod.GET)
+	public ResponseEntity<?> checkOrderStatus(@PathVariable("id") String orderId) {
+		return ResponseEntity.ok("{'status' : 'shipped'}");
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getOrder(@PathVariable("id") String orderId) {
+		Item book1 = new Item("101", 1);
+		Item book2 = new Item("103", 5);
+		PaymentMethod myvisa = new PaymentMethod("VISA", "01/22", "John Doe", "201, 1st Street, San Jose, CA");
+		Order order = new Order("101021", orderId, myvisa, new Item[] { book1, book2 },
+				"201, 1st Street, San Jose, CA");
+		return ResponseEntity.ok(order);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<?> createOrder(@RequestBody Order order) {
+
+		if (order != null) {
+			RestTemplate restTemplate = new RestTemplate();
+			URI uri = URI.create("https://localhost:8443/inventory");
+			restTemplate.put(uri, order.getItems());
+
+			order.setOrderId(UUID.randomUUID().toString());
+			URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(order.getOrderId()).toUri();
+
+			return ResponseEntity.created(location).build();
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+}
---
+++ chapter06/sample02/src/main/java/com/manning/mss/ch06/sample02/service/Inventory.java
@@ -0,0 +1,28 @@
+package com.manning.mss.ch06.sample02.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.manning.mss.ch06.sample02.model.Item;
+
+@RestController
+@RequestMapping(value = "/inventory")
+public class Inventory {
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public ResponseEntity<?> updateItems(@RequestBody Item[] items) {
+
+		if (items == null || items.length == 0) {
+			return ResponseEntity.badRequest().build();
+		}
+		for (Item item : items) {
+			if (item != null) {
+				System.out.println(item.getCode());
+			}
+		}
+		return ResponseEntity.noContent().build();
+	}
+}
