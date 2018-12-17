b7fe14b64746c2d159a569e55379db518bb9d719
Prabath Siriwardana <prabath@wso2.com>
2018-12-17 05:46:49 -0800

    updated samples

--- chapter07/sample04/src/main/java/com/manning/mss/ch07/sample04/service/OrderProcessingService.java
+++ chapter07/sample04/src/main/java/com/manning/mss/ch07/sample04/service/OrderProcessingService.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
