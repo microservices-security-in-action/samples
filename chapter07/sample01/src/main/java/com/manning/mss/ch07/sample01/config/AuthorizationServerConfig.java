d83c1b864d13fa359bf8ca131430dcad6931f411
prabath <prabath@wso2.com>
2019-10-27 14:55:57 -0700

    updates

--- chapter07/sample01/src/main/java/com/manning/mss/ch07/sample01/config/AuthorizationServerConfig.java
+++ chapter07/sample01/src/main/java/com/manning/mss/ch07/sample01/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.manning.mss.ch07.sample01.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,8 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -25,12 +29,22 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private Environment environment;
 
+	@Bean
+	public TokenEnhancer tokenEnhancer() {
+		return new CustomJWTEnhancer();
+	}
+
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		String useJwt = environment.getProperty("spring.security.oauth.jwt");
 		if (useJwt != null && "true".equalsIgnoreCase(useJwt.trim())) {
-			endpoints.tokenStore(tokenStore()).tokenEnhancer(jwtConeverter())
+
+			TokenEnhancerChain enhancerChain = new TokenEnhancerChain();
+			enhancerChain.setTokenEnhancers(Arrays.asList(tokenEnhancer(), jwtConeverter()));
+			endpoints.tokenStore(tokenStore()).accessTokenConverter(jwtConeverter()).tokenEnhancer(enhancerChain)
 					.authenticationManager(authenticationManager);
+			// endpoints.tokenStore(tokenStore()).tokenEnhancer(jwtConeverter())
+			// .authenticationManager(authenticationManager);
 		} else {
 			endpoints.authenticationManager(authenticationManager);
 		}
@@ -44,7 +58,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("applicationid").secret("applicationsecret").scopes("foo", "bar")
-				.authorizedGrantTypes("client_credentials", "password", "refresh_token").accessTokenValiditySeconds(6000);
+				.authorizedGrantTypes("client_credentials", "password", "refresh_token")
+				.accessTokenValiditySeconds(6000);
 	}
 
 	@Bean
