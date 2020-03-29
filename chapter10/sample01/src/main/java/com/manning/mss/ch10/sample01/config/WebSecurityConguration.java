2e12cf76486fbb25746c1dcbe50ef6e7752e34ab
Nuwan Dias <nuwand@wso2.com>
2020-03-29 07:42:22 +0530

    Updating spring versions for sampple 1 in chapter 10

--- chapter10/sample01/src/main/java/com/manning/mss/ch10/sample01/config/WebSecurityConguration.java
+++ chapter10/sample01/src/main/java/com/manning/mss/ch10/sample01/config/WebSecurityConguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class WebSecurityConguration extends WebSecurityConfigurerAdapter {
@@ -18,7 +19,15 @@ public class WebSecurityConguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("peter").password("peter123").roles("USER");
+		auth.inMemoryAuthentication()
+				.withUser("peter")
+				.password(passwordEncoder().encode("peter123"))
+				.roles("USER");
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 }
