c75cf7fba7ba27d21a6777e971e61560065f14e0
prabath <prabath@wso2.com>
2020-04-12 18:59:47 -0700

    updates

--- appendix-g/sample01/run_opa.sh
+++ appendix-g/sample01/run_opa.sh
@@ -1,1 +1,2 @@
-docker run "$(pwd)"/policies:/policies -p 8181:8181 openpolicyagent/opa:0.15.0 run /policies --server
+docker run --mount type=bind,source="$(pwd)"/policies,target=/policies \
+-p 8181:8181 openpolicyagent/opa:0.15.0 run /policies --server
