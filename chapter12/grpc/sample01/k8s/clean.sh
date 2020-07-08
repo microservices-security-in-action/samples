782b8d904bdaed12c63d8d16df88d0a0596dad03
prabath <prabath@wso2.com>
2020-07-07 18:27:28 -0700

    updates

--- chapter12/grpc/sample01/k8s/clean.sh
+++ chapter12/grpc/sample01/k8s/clean.sh
@@ -3,8 +3,8 @@ kubectl delete svc inventory-service orders-service sts-service
 kubectl delete virtualservices ecomm-virtual-service orders-virtual-service sts-virtual-service orders-virtual-service
 kubectl delete deployment inventory-deployment orders-deployment sts-deployment
 kubectl delete gateway ecomm-gateway -n istio-system 
-kubectl delete authorizationpolicy inventory-services-policy
-kubectl delete requestauthentication inventory-req-authn-policy
+kubectl delete authorizationpolicy orders-services-policy inventory-services-policy
+kubectl delete requestauthentication orders-req-authn-policy inventory-req-authn-policy
 kubectl delete destinationrule disable-mtls -n istio-system
 kubectl delete configmaps orders-application-properties-config-map inventory-application-properties-config-map sts-application-properties-config-map sts-jwt-keystore-config-map
 kubectl delete secret sts-keystore-secrets
